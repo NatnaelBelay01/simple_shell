65559020e2a6ff09a7dd109a86ec2127ecf24684
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-06-15 01:28:54 -0700

    re

--- shell.c
+++ shell.c
@@ -1,46 +1,150 @@
 #include "main.h"
-/**
- * main - the main fucntion
- * Return: always 0
- */
-int main(void)
-{
-	char *av[100], *str = NULL;
-	int status = 0, len = 0;
-	pid_t child;
 
-	while (1)
+/**
+ * shell - simple shell
+ * @build: input build
+ */
+void shell(config *build)
+{
+	while (true)
 	{
-		if (child != 0 || status == 0)
-		{
-			write(1, "$ ", 2);
-			status = 1;
-		}
-		len = input(&str);
-		if (len == -1)
-			break;
-		if (str[0] == '\n')
-		{
-			status = 0;
+		checkAndGetLine(build);
+		if (splitString(build) == false)
 			continue;
-		}
-		str[len - 1] = '\0';
-		if (ex_check(str) == 1)
-			break;
-		str = edit(str);
-		parser(str, av);
-		child = fork();
-		if (child == 0)
+		if (findBuiltIns(build) == true)
+			continue;
+		checkPath(build);
+		forkAndExecute(build);
+	}
+}
+
+/**
+ * checkAndGetLine - check stdin and retrieves next line; handles
+ * prompt display
+ * @build: input build
+ */
+void checkAndGetLine(config *build)
+{
+	register int len;
+	size_t bufferSize = 0;
+	char *ptr, *ptr2;
+
+	build->args = NULL;
+	build->envList = NULL;
+	build->lineCounter++;
+	if (isatty(STDIN_FILENO))
+		displayPrompt();
+	len = getline(&build->buffer, &bufferSize, stdin);
+	if (len == EOF)
+	{
+		freeMembers(build);
+		if (isatty(STDIN_FILENO))
+			displayNewLine();
+		if (build->errorStatus)
+			exit(build->errorStatus);
+		exit(EXIT_SUCCESS);
+
+	}
+	ptr = _strchr(build->buffer, '\n');
+	ptr2 = _strchr(build->buffer, '\t');
+	if (ptr || ptr2)
+		insertNullByte(build->buffer, len - 1);
+	stripComments(build->buffer);
+}
+
+/**
+ * stripComments - remove comments from input string
+ * @str: input string
+ * Return: length of remaining string
+ */
+void stripComments(char *str)
+{
+	register int i = 0;
+	_Bool notFirst = false;
+
+	while (str[i])
+	{
+		if (i == 0 && str[i] == '#')
 		{
-			if (execve(av[0], av, environ) == -1)
+			insertNullByte(str, i);
+			return;
+		}
+		if (notFirst)
+		{
+			if (str[i] == '#' && str[i - 1] == ' ')
 			{
-				perror(av[0]);
-				break;
+				insertNullByte(str, i);
+				return;
 			}
 		}
-		else
-			wait(0);
+		i++;
+		notFirst = true;
 	}
-	free(str);
-	return (0);
+}
+
+/**
+ * forkAndExecute - fork current build and execute processes
+ * @build: input build
+ */
+void forkAndExecute(config *build)
+{
+	int status;
+	pid_t f1 = fork();
+
+	convertLLtoArr(build);
+	if (f1 == -1)
+	{
+		perror("error\n");
+		freeMembers(build);
+		freeArgs(build->envList);
+		exit(1);
+	}
+	if (f1 == 0)
+	{
+		if (execve(build->fullPath, build->args, build->envList) == -1)
+		{
+			errorHandler(build);
+			freeMembers(build);
+			freeArgs(build->envList);
+			if (errno == ENOENT)
+				exit(127);
+			if (errno == EACCES)
+				exit(126);
+		}
+	} else
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			build->errorStatus = WEXITSTATUS(status);
+		freeArgsAndBuffer(build);
+		freeArgs(build->envList);
+	}
+}
+
+/**
+ * convertLLtoArr - convert linked list to array
+ * @build: input build
+ */
+void convertLLtoArr(config *build)
+{
+	register int i = 0;
+	size_t count = 0;
+	char **envList = NULL;
+	linked_l *tmp = build->env;
+
+	count = list_len(build->env);
+	envList = malloc(sizeof(char *) * (count + 1));
+	if (!envList)
+	{
+		perror("Malloc failed\n");
+		exit(1);
+	}
+	while (tmp)
+	{
+		envList[i] = _strdup(tmp->string);
+		tmp = tmp->next;
+		i++;
+	}
+	envList[i] = NULL;
+	build->envList = envList;
 }
